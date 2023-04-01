cfaa2579ec0df32f2fe0064211726133a202affd
victoryang00 <yangyiwei2000@gmail.com>
2023-04-01 12:04:18 -0700

    remove allocator

--- allocator/alloc.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by victoryang00 on 2/2/23.
-//
-
-#include "alloc.h"
--- allocator/opt_api/include/internal/smalloc.h
+++
@@ -1,6 +0,0 @@
-#ifndef SMALLOC_H_
-#define SMALLOC_H_
-
-extern int init_smalloc(void);
-
-#endif /* SMALLOC_H_ */
